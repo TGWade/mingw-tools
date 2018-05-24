72f3353cae155271afb4ae4da7dfdd596b447589
wadet2 <timothy.wade@emc.com>
2018-05-24 08:43:55 -0400

    cleanup

--- src/rsync.sh
+++ src/rsync.sh
@@ -1,9 +1,12 @@
 #! /bin/bash
 
 # This script will allow the use of rsync and accommodate for the problem with using the
-# windows drive in the path, i.e. "c:/somepath" which rsync
+# windows drive in the path with a ":", i.e. "c:/somepath" which rsync
 # interprets as a hostname/ip.  Use /<drive>/ instead, i.e. "/c/somepath" with this script.
+
 PROG=$(basename $0)
+# set MINGW_RSYNC in environment to override
+[ -z "$MINGW_RSYNC" ] && MINGW_RSYNC=/c/MinGW/msys/1.0/bin/rsync.exe
 
 # There is an issue with pathnames with escaped backspaces which is not
 # handeled with just "$*", but is with an array
@@ -12,17 +15,14 @@ while [ "$1" ]; do
     largs+=( "$1" )
     shift
 done
-#echo ${PROG}: myargs ${largs[@]}
 
 # This handles the drive issue
 export MSYS2_ARG_CONV_EXCL="*"
-cmd=/c/MinGW/msys/1.0/bin/rsync.exe
-echo "RUNNING: $cmd ${largs[@]}"
-#/c/MinGW/msys/1.0/bin/rsync.exe "${largs[@]}"
-$cmd "${largs[@]}"
+echo "RUNNING: $MINGW_RSYNC ${largs[@]}"
+$MINGW_RSYNC "${largs[@]}"
 
 [ "$?" -eq 0 ] && exit 0
 echo '--- NOTE! ---'
-echo 'You received an error. If you used the drive in the path (i.e. "c:/somepath"), '
+echo 'You received an error. If you used the drive in the path, with a ":" (i.e. "c:/somepath"), '
 echo 'try using "/<drive>/" instead (i.e. "/c/somepath")'
 
